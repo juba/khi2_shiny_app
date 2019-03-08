99e7ddf033e44b9db5d1a518aeeaf91b1bedc24d
Julien <julien@nozav.org>
2019-03-08 15:58:31 +0100

    Recodage partiel `clso`

--- global.R
+++ global.R
@@ -3,3 +3,6 @@
 #d <- hdv2003
 load("www/data/hdv2003.Rdata")
 d.vars <- names(d)
+d$clso <- as.character(d$clso)
+d$clso[d$clso == "1"] <- "Oui"
+d$clso <- factor(d$clso, levels = c("Oui", "Non", "Ne sait pas"))
